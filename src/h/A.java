215c5ffbf78c7b3440aca04afd252338a15064a5
zoulinjiang <95562831+zoulinjiang@users.noreply.github.com>
2021-12-25 18:57:58 +0800

    Update A.java

--- src/h/A.java
+++ src/h/A.java
@@ -2,12 +2,7 @@ package h;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("ac");
-        System.out.println("cacv");
-        System.out.println("dadaf");
-        System.out.println("avvv");
-        System.out.println("啊哒哒哒");
-        System.out.println("阿达");
-        System.out.println("vv并不被");
+        System.out.println("扑棱棱1");
+    
     }
 }
